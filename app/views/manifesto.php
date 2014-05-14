995d1a3c7d6efdc2e4462b1c95f20d3ae94944c2
Kayla Daniels <kayladnls@gmail.com>
2014-05-14 06:28:42 +0000

    first commit

---
+++ app/views/manifesto.php
@@ -0,0 +1,95 @@
+<html lang="en"><head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>The Code Manifesto</title>
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+
+
+    <!-- Custom styles for this template -->
+    <link href="/css/manifesto.css" rel="stylesheet">
+
+  
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  <style id="holderjs-style" type="text/css"></style><link type="text/css" rel="stylesheet" href="chrome-extension://cpngackimfmofbokmjmljamhdncknpmg/style.css"><script type="text/javascript" charset="utf-8" src="chrome-extension://cpngackimfmofbokmjmljamhdncknpmg/js/page_context.js"></script></head>
+
+  <body screen_capture_injected="true">
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <!--<ul class="nav masthead-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>-->
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">Code Manifesto</h1>
+            <p >We want to operate in an ecosystem that empowers developers to reach their potential. One that encourage growth and collaboration. A space that is safe for all.</p>
+            <p >
+             A space such as this benefits everyone that participates in it. It encourages new developers to enter our field. It is through discussion and collaboration that we grow, and through growth that we improve. 
+            </p>
+			<hr />
+			
+			<h4>
+            In the effort to create such a place, we hold to these values
+            </h4>
+			<ol>
+				<li>
+				<strong>Discrimination limits us.</strong> This includes discrimination on the basis of race, gender, sexual orientation, nationality and any other arbitrary exclusion of a group of people.
+				</li>
+				<li>
+				<strong>Boundaries honor us.</strong> Your comfort levels are not everyone’s comfort levels. Remember that, and if brought to your attention, heed it.
+
+				</li>
+				<li>
+				<strong>We are our biggest assets.</strong> None of us were born masters of our trade. Each of us has been helped along the way. Return that favor, when and where you can. 
+				</li>
+				<li>
+				<strong>We are resources for the future.</strong> As an extension of #3, share what you know. Make yourself a resource to help those that come after you. 
+				</li>
+				<li>
+				<strong>Respect defines us.</strong> Treat others as you wish to be treated. Make your discussions, criticisms and debates from a position of respectfulness. Ask yourself, is it true, is it necessary, is it constructive? Anything less is untolerated. 
+				</li>
+			</ol>
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+             
+			 </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+  
+<!-- Latest compiled and minified JavaScript -->
+<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+
+</body></html>
