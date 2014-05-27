1770cfc97c74799aa69eca533c76a4dff8744db5
aalaap <aalaap@gmail.com>
2014-05-27 20:14:21 +0530

    Added "View List" to main menu
    Temporary direct link to show supports by showing the thank you page directly.

--- app/views/includes/menu.blade.php
+++ app/views/includes/menu.blade.php
@@ -4,8 +4,9 @@
             <h3 class="masthead-brand">Code Manifesto</h3>
         </a>
         <ul class="nav masthead-nav">
-            <li><a href="/support">Support</a></li>
             <li><a href="/about">About</a></li>
+            <li><a href="/thankYou">View List</a></li>
+            <li><a href="/support">Support</a></li>
 <!--            <li><a href="/developers">Developers</a></li>-->
 <!--            <li><a href="/businesses">Businesses</a></li>-->
 <!--            <li><a href="/conferences">Conferences</a></li>-->
