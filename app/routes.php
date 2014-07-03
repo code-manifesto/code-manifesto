b8c29f8cd16ab5957d2944a7677344a7cf74f394
Kayla Daniels <kayladnls@gmail.com>
2014-07-02 22:25:52 -0400

    cleaned up routes file, created ManifestoController

--- app/routes.php
+++ app/routes.php
@@ -1,45 +1,13 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+Route::get('/ln/{language}', 'ManifestoController@manifesto');
 
-Route::get('/ln/{language}', function($language)
-{
-    $Parsedown = new Parsedown();
-    $content = File::get(base_path()."/README.".$language.".md");
-    $content = $Parsedown->text($content);
-    return View::make('manifesto')->with(array('active_item' => 'home', 'content' => $content));
-});
+Route::get('/', 'ManifestoController@manifesto');
 
-Route::get('/', function()
-{
-    $Parsedown = new Parsedown();
-    $content = File::get(base_path()."/README.md");
-    $content = $Parsedown->text($content);
-    return View::make('manifesto')->with(array('active_item' => 'home', 'content' => $content));
-});
+Route::get('/about', 'ManifestoController@about');
 
-Route::get('/support', function()
-{
-	return View::make('support')->with(array('active_item' => 'support'));
-});
+Route::get('/support', 'SupporterController@support');
 
-Route::post('/support', array('uses' => 'SupporterController@newSupporter'));
+Route::post('/support', 'SupporterController@newSupporter');
 
-Route::get('/thankYou', function()
-{
-    return View::make('thank_you')->with(array('supporters'=> Supporter::all(), 'active_item'=>'list'));
-});
-
-Route::get('/about', function()
-{
-    return View::make('about')->with(array('active_item' => 'about'));
-});
+Route::get('/thankYou', 'SupporterController@thankYou');
