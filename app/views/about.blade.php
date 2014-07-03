8c5b54cee4a151fe26daee789aaaef922cef2c30
Kayla Daniels <kayladnls@gmail.com>
2014-07-03 00:06:51 -0400

    added attribution.

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -28,6 +28,9 @@
     <br/>
     <br/>
     Logo Designed by <a href="http://matthewa.com/" target="_blank">Matt Andrews</a><br><br>
+    <br/>
+    <br/>
+    Front End Contributions by <a href="https://github.com/craigpaul">Craig Paul</a>
 
     <h4>Translations</h4>
     Arabic by <a href="https://github.com/badrchaouai">Badr Chaouai</a><br>
