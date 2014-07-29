cc0f73ce55927df356adc273875b8df262f7b4b7
Craig Paul <craig.paul91@gmail.com>
2014-07-29 13:44:48 +0000

    Added environment detection

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,11 +24,10 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(array(
-
-	'local' => array('your-machine-name'),
-
-));
+$env = $app->detectEnvironment(function()
+{
+	return getenv('APP_ENV') ?: 'local';
+});
 
 /*
 |--------------------------------------------------------------------------
